7ae0263217caece836dd973049e427d2f16e6d0f
santiloboc <slobod@uninorte.edu.co>
2026-05-21 15:09:00 -0500

    agregar enums de dominio en core/models

---
+++ src/core/models/HospitalizationStatus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package core.models;
+
+/**
+ *
+ * @author edangulo
+ */
+public enum HospitalizationStatus {
+
+    REQUESTED,
+    ONGOING,
+    CANCELED
+
+}
---
+++ src/core/models/Specialty.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package core.models;
+
+/**
+ *
+ * @author edangulo
+ */
+public enum Specialty {
+
+    GENERAL_MEDICINE,
+    CARDIOLOGY,
+    PEDIATRICS,
+    NEUROLOGY,
+    TRAUMATOLOGY_ORTHOPEDICS,
+    GYNECOLOGY_OBSTETRICS,
+    DERMATOLOGY,
+    PSYCHIATRY,
+    ONCOLOGY,
+    OPHTHALMOLOGY,
+    INTERNAL_MEDICINE
+
+}
