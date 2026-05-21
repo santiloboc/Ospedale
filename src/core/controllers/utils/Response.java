84abdbf8f77ad1341fa56a9e119fafb93b2750cf
santiloboc <slobod@uninorte.edu.co>
2026-05-20 19:23:08 -0500

    chore: estructura del paquete MVC en src/

---
+++ src/core/controllers/utils/Response.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.controllers.utils;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author edangulo
+ */
+public class Response {
+
+    private String message;
+    private int status;
+    private HashMap<String, Object> data;
+
+    public Response(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public Response(String message, int status, HashMap<String, Object> data) {
+        this.message = message;
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+}
